fb33d76c3009e3323bbb36fcd93b7cd42b38a35b
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2021-09-22 20:29:42 +0300

    Working buttons

--- Assets/Scripts/Inventory/Playerinventory.cs
+++ Assets/Scripts/Inventory/Playerinventory.cs
@@ -1,12 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playerinventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    Event e;
+    KeyCode keyCode;
 
+
+    private void OnGUI()
+    {
+        e = Event.current;
+        if (e.isKey)
+        {
+            keyCode = e.keyCode;
+        }
+    }
+    private void Update()
+    {
+
+        switch (keyCode)
+        {
+            case KeyCode.Alpha1:
+
+                slots[0].GetComponentInChildren<Button>()?.onClick.Invoke();
+                keyCode = 0;
+                break;
+            case KeyCode.Alpha2:
+                slots[1].GetComponentInChildren<Button>()?.onClick.Invoke();
+                keyCode = 0;
+                break;
+            case KeyCode.Alpha3:
+                slots[2].GetComponentInChildren<Button>()?.onClick.Invoke();
+                keyCode = 0;
+                break;
+        }
+    }
 
     private void Start()
     {
