fd49aa68a2082c51bb8ba1f07574c49dd39b54f4
osher volotker <volotkero@gmail.com>
2021-10-20 16:19:30 +0300

    fixed bugs

---
+++ Assets/Scripts/Platform movement/PlatformY.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformY : MonoBehaviour
+{
+    public float upLimit = 2.5f;
+    public float downLimit = 1.0f;
+    public float speed = 2.0f;
+    private int direction = 1;
+    Vector3 movement;
+
+    void Update()
+    {
+        if (transform.position.y > upLimit)
+        {
+            direction = -1;
+        }
+        else if (transform.position.y < downLimit)
+        {
+            direction = 1;
+        }
+        movement = Vector3.up * direction * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
+
